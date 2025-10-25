045feb671a6eb930017101e3b94d31431ad6c3d1
YoussefSaadfcis <www.yosefsaad4102002@gmail.com>
2025-10-26 02:53:00 +0300

    add auth files and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\auth\LogOutController;
+use App\Http\Controllers\auth\RegisterController;
+use App\Http\Controllers\auth\SignInController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("auth")->group(function () {
+    Route::get("signup", [RegisterController::class, "show_signup_page"])->name("auth.signup.get");
+    Route::post("signup", [RegisterController::class, "login"])->name("auth.signup.post");
+
+    Route::get("signin", [SignInController::class, "show_signin_page"])->name("auth.signin.get");
+    Route::post("signin", [SignInController::class, "signin"])->name("auth.signin.post");
+
+    Route::post("logout", [LogOutController::class, "logout"])->name("auth.logout.post");
 });
